09eb84c4b1bded9263d6e5fd53ff1edbb9b27f12
KOREAVC <KOREAVC@DESKTOP-GJSDUCF>
2024-01-15 18:13:25 +0900

    2차 프로젝트 초안

---
+++ CampBridge2024/src/main/java/com/java/www/controller/FController.java
@@ -0,0 +1,21 @@
+package com.java.www.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FController {
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}// index()
+	
+	@GetMapping("nList")
+	public String nList() {
+		return "nList";
+	}// nList()
+	
+	
+
+}// FController
